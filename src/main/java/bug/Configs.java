e310d1c503c66d3d1169e2e66a1b8cabda67622d
Marcus Hert Da Coregio <marcusdacoregio@gmail.com>
2022-07-22 11:31:09 -0300

    Create SecurityFilterChain beans

--- src/main/java/bug/Configs.java
+++ src/main/java/bug/Configs.java
@@ -25,105 +25,50 @@ public class Configs {
 
     public static final String REST_REALM = "REST realm";
 
-    @Configuration
+    @Bean
+    @Order(0)
+    public SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .antMatcher("/rest/**/**").sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/rest/login", "/rest/v1/public/**/**").permitAll()
+                .antMatchers("/rest/**/**").authenticated()
+                .and()
+                .antMatcher("/rest/**/**").httpBasic().realmName(REST_REALM)
+                .and()
+                .antMatcher("/rest/**/**").csrf().disable().headers().cacheControl();
+
+        return http.build();
+    }
+
+    @Bean
     @Order(1)
-    @Profile("profile26")
-    public static class ConfigRest26 extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http
-                    .antMatcher("/rest/**/**").sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/rest/login", "/rest/v1/public/**/**").permitAll()
-                    .antMatchers("/rest/**/**").authenticated()
-                    .and()
-                    .antMatcher("/rest/**/**").httpBasic().realmName(REST_REALM)
-                    .and()
-                    .antMatcher("/rest/**/**").csrf().disable().headers().cacheControl();
-
-        }
-    }
-
-    @Configuration
-    @Profile("profile26")
-    public static class Config26 extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http
-                    .headers().frameOptions().sameOrigin()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/manager/html").denyAll()
-                    .antMatchers("/static/**/**", "/css/**", "/js/**", "/webjars/**").permitAll()
-                    .antMatchers("/favicon.ico", "/", "/index", "/content/public/**/**", "/login", "/jssc", "/error/**").permitAll()
-                    .antMatchers("/login").anonymous()
-                    .antMatchers("/logout", "/content/secured/**/**").authenticated()
-                    .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                    .antMatchers("/user/**").hasAnyRole("USER")
-                    .antMatchers("/**/**").denyAll()
-                    .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("j_username")
-                    .passwordParameter("j_password")
-                    .loginProcessingUrl("/jssc")
-                    .defaultSuccessUrl("/index")
-                    .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/index");
-
-        }
-    }
-
-    @Configuration
-    @Profile("profile27")
-    public static class Config27{
-
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-            http
-                    .antMatcher("/rest/**/**").sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/rest/login", "/rest/v1/public/**/**").permitAll()
-                    .antMatchers("/rest/**/**").authenticated()
-                    .and()
-                    .antMatcher("/rest/**/**").httpBasic().realmName(REST_REALM)
-                    .and()
-                    .antMatcher("/rest/**/**").csrf().disable().headers().cacheControl();
-
-            http
-                    .headers().frameOptions().sameOrigin()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/manager/html").denyAll()
-                    .antMatchers("/static/**/**", "/css/**", "/js/**", "/webjars/**").permitAll()
-                    .antMatchers("/favicon.ico", "/", "/index", "/content/public/**/**", "/login", "/jssc", "/error/**").permitAll()
-                    .antMatchers("/login").anonymous()
-                    .antMatchers("/logout", "/content/secured/**/**").authenticated()
-                    .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                    .antMatchers("/user/**").hasAnyRole("USER")
-                    .antMatchers("/**/**").denyAll()
-                    .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("j_username")
-                    .passwordParameter("j_password")
-                    .loginProcessingUrl("/jssc")
-                    .defaultSuccessUrl("/index")
-                    .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/index");
-
-            return http.build();
-        }
+    public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/manager/html").denyAll()
+                .antMatchers("/static/**/**", "/css/**", "/js/**", "/webjars/**").permitAll()
+                .antMatchers("/favicon.ico", "/", "/index", "/content/public/**/**", "/login", "/jssc", "/error/**").permitAll()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/logout", "/content/secured/**/**").authenticated()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .antMatchers("/**/**").denyAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                .loginProcessingUrl("/jssc")
+                .defaultSuccessUrl("/index")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/index");
+        return http.build()
     }
 }
